1646528dbe570bf48df83d91ccd493995c0fec5d
siddikur-dev <siddikur.dev@gmail.com>
2025-10-07 20:53:06 +0200

    navber designed with responsived

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,98 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router";
+import Button from "../Button/Button";
+import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
+  const links = [
+    { name: "Home", path: "/" },
+    { name: "Apps", path: "/apps" },
+    { name: "Installation", path: "/installation" },
+  ];
+
+  return (
+    <div className="bg-base-300">
+      <div className="navbar px-4 md:px-8 container mx-auto">
+        {/* Navbar Start */}
+        <div className="navbar-start">
+          {/* Mobile Menu */}
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className=" menu-sm dropdown-content bg-base-100 rounded-box mt-3 w-52 p-2 shadow z-10"
+            >
+              {links.map((link) => (
+                <li key={link.path}>
+                  <NavLink
+                    to={link.path}
+                    className={({ isActive }) =>
+                      isActive
+                        ? "font-semibold bg-gradient-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent"
+                        : "font-medium text-gray-700 hover:text-[#632EE3]"
+                    }
+                  >
+                    {link.name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {/* Brand Name */}
+          <NavLink
+            to="/"
+            className="lg:text-2xl font-bold bg-gradient-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent"
+          >
+            Hero Store
+          </NavLink>
         </div>
-    );
+
+        {/* Navbar Center (Desktop) */}
+        <div className="navbar-center hidden lg:flex">
+          <ul className=" menu-horizontal px-1 gap-4">
+            {links.map((link) => (
+              <li key={link.path}>
+                <NavLink
+                  to={link.path}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "bg-gradient-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent font-bold underline"
+                      : "font-medium text-gray-700 hover:text-[#632EE3] transition "
+                  }
+                >
+                  {link.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Navbar End */}
+        <div className="navbar-end">
+          <Button className="flex items-center gap-2">
+            <FaGithub className="text-lg" />
+            Contribute
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
